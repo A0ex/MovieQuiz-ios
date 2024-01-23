a512b32e5923a8dab5f63e4ce8f4a01d72cd47e4
A0ex <alxslvrstv@yandex.ru>
2024-01-23 11:35:19 +0300

    Переделана структура проекта
    Добавлены папки Models (QuizStepViewModel.swift, QuizResultsViewModel.swift, QuizQuestion.swift) и Services (QuestionFactory.swift), настроена логика работы

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,8 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by AlexS on 22.01.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactory.swift
+//  MovieQuiz
+//
+//  Created by AlexS on 22.01.2024.
+//
+
+import Foundation
