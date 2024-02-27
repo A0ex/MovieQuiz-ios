08491200283871919d6c66a722568e584c2f641f
A0ex <alxslvrstv@yandex.ru>
2024-02-27 19:13:31 +0300

    Убран лишний закомментированный код

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -25,19 +25,3 @@ final class AlertPresenter {
     }
 }
 
-//final class AlertPresenter {
-//    func show(in vc: UIViewController, model: AlertModel) {
-//        let alert = UIAlertController(
-//            title: model.title,
-//            message: model.message,
-//            preferredStyle: .alert)
-//
-//        let action = UIAlertAction(title: model.buttonText, style: .default) { _ in
-//            model.completion()
-//        }
-//
-//        alert.addAction(action)
-//
-//        vc.present(alert, animated: true, completion: nil)
-//    }
-//}
